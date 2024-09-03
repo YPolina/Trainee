bf54f712493e0958b18f6c58525482ba2e396fc1
«YPolina» <yatsko_polina1@mail.ru>
2024-09-03 13:34:49 +0300

    updates

--- etl.py
+++ etl.py
@@ -1,17 +1,39 @@
 import pandas as pd
 from pandas.core.frame import DataFrame
 
-dir_ = './competitive-data-science-predict-future-sales'
 
+class ELTPipline():
 
-def exctraction(file_path: str) -> DataFrame:
-    return pd.read_csv(f'{dir_}/{file_path}')
+    def __init__(self):
+        self.dir_ = './competitive-data-science-predict-future-sales'
 
-def transform(self, df: DataFrame) -> DataFrame:
+    def exctraction(self, file_path: str) -> DataFrame:
+        return pd.read_csv(f'{self.dir_}/{file_path}')
 
-    df.dropna(inplace= True)
+    def transform(self, df: DataFrame) -> DataFrame:
 
-    return df
+        if not df.isna().values.any():
+            df.dropna(inplace=True)
+
+        if df.duplicated().values.any():
+            df.drop_duplicates(inplace=True)
+
+        def outliers(self, df: DataFrame, column: str) -> DataFrame:
+
+            Q1 = df[column].quantile(0.25)
+            Q3 = df[column].quantile(0.75)
+            IQR = Q3 - Q1
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+            outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
+            return outliers
+        
+        
+        for column in df.columns:
+            df.drop(df[df[outliers(df, column)] == True].index, inplace=True)
+
+        return df
+            
 
 
 '''
