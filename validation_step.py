b4d9ce7abcdd89f02ad5c28032c48760125fced3
«YPolina» <yatsko_polina1@mail.ru>
2024-10-20 14:00:34 +0300

    DS-2.1 Baseline modeling Changes: -	Feature engineering: revenue, shop_history, minor_category_history -	Construction of full data with all possible item&shop pairs -	Lags features -	Log transformation for target (item_cnt_month) and for revenue and item_price features -	Categorical encoding

--- validation_step.py
+++ validation_step.py
@@ -1,11 +1,180 @@
+import pandas as pd
+import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
+#Feature engineering
+class FeatureEngineering():
+
+    def __init__(self, target_col = 'item_cnt_month', item_price = 'item_price'):
+        self.target_col = target_col
+        self.item_price = item_price
+
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X):
+
+        #Revenue - item_cnt_month * item_price
+        X['revenue'] = X[self.target_col] * X[self.item_price]
+
+        #Shop history: To track amount of time blocks with data for each shop
+        X['shop_history'] = (X.groupby('shop_id')['date_block_num'].transform('nunique'))
+        #Minor_category_history: To track amount of time blocks with data for each minor_category
+        X['minor_category_history'] = (X.groupby('minor_category_id')['date_block_num'].transform('nunique'))
+
+        return X
+
+#To ensure that our df has all shop&item combinations
+class FullDataframeCreation(BaseEstimator, TransformerMixin):
+
+    def __init__(self, date_block_num):
+        self.date_block_num = date_block_num
+
+    def fit(self, X, y = None):
+
+        return self
+
+    def transform(self, X):
+
+        #All unique values of existed shops and items
+        full_data_list = []
+        shop_ids = X['shop_id'].unique()
+        item_ids = X['item_id'].unique()
+        
+        #Iterating through unique values to get all shop&item combinations
+        for i in range(self.date_block_num + 1):
+            for shop in shop_ids:
+                for item in item_ids:
+                    full_data_list.append([i, shop, item])
+        columns = ['date_block_num','shop_id','item_id']
+        full_data = pd.DataFrame(full_data_list, columns=columns)
+        full_data.sort_values(by = columns, inplace = True)
+        
+        X = pd.merge(full_data, X, on = columns, how = 'left')
+        
+        #Merging all combinations with original dataset
+        return X
+
+
+#Lag features for target column
+class LagFeatureGenerator(BaseEstimator, TransformerMixin):
+
+    def __init__(self, target_col = 'item_cnt_month', group_cols = ['date_block_num', 'shop_id', 'item_id'], lags = [1,2,3,6,12]):
+        self.lags = lags
+        self.group_cols = group_cols
+        self.target_col = target_col
+
+    def fit(self, X, y = None):
+        return self
+
+    def transform(self, X):
+        
+        
+        for lag in self.lags:
+            X[f'{self.target_col}_lag_{lag}'] = X.groupby(self.group_cols)[self.target_col].shift(lag)
+        X = X.fillna(0)
+        return X
+
+def pipeline_1(date_block_num, lags):
+
+    pipeline_1 = Pipeline(steps = [
+        ('feature engineering', FeatureEngineering()),
+        ('full set', FullDataframeCreation(date_block_num)),
+        ('lag features', LagFeatureGenerator(lags = lags))
+    ])
+
+
+
+    return pipeline_1
+
+
+#Log Transformations
+class LogTransformer(BaseEstimator, TransformerMixin):
+
+    def __init__(self, columns):
+        self.columns = columns
+    
+    def fit(self, X, y = None):
+        return self
+
+    def transform(self, X):
+        
+        for col in self.columns:
+            X[col] = np.log1p(X[col])
+        
+        return X
+
+
+
+#To handle encoding column by column
+class MulticolumnsLabelEncoding(BaseEstimator, TransformerMixin):
+
+    def __init__(self, columns = None):
+        self.columns = columns
+        #To store encoders for each column
+        self.encoders = {}
+
+    #Fit each column separatly
+    def fit(self, X, y = None):
+
+        if not isinstance(X, pd.DataFrame):
+            X = pd.DataFrame(X, columns=self.columns)
+        for col in self.columns:
+            le = LabelEncoder()
+            le.fit(X[col].astype(str))
+            self.encoders[col] = le
+        
+        return self
+    
+    #Apply Label Encoder for each column
+    def transform(self, X):
+        if not isinstance(X, pd.DataFrame):
+            X = pd.DataFrame(X, columns=self.columns)
+        
+        for col in self.columns:
+            X[col] = self.encoders[col].transform(X[col].astype(str))
+        
+        return X
+
+
+#All steps
+def pipeline_2(log_transform_cols, categorical_columns):
+
+    categorical_transformer = Pipeline(steps = [
+        ('encoding', MulticolumnsLabelEncoding(columns=categorical_columns))
+    ])
+
+    log_transformer = Pipeline(steps = [
+        ('log_transform', LogTransformer(columns = log_transform_cols))
+    ])
+
+
+    preprocessor = ColumnTransformer(
+        transformers = [
+            ('log', log_transformer, log_transform_cols),
+            ('cat', categorical_transformer, categorical_columns)
+        ]
+    )
+
+    pipeline_2 = Pipeline(steps = [
+        ('preprocessor', preprocessor)
+    ])
+
+
+
+    return pipeline_2
+
 
 
 #Validation
 class Validator(BaseEstimator, TransformerMixin):
 
     
-    def __init__(self, column_types = None, value_ranges = None, non_negative_values = None, check_duplicates = True, check_missing = True):
+    def __init__(self, column_types = None, value_ranges = None, non_negative_columns = None, check_duplicates = True, check_missing = True):
         #column types:dict
         #Expected data type for each column {'shop_id': 'int64'}
         self.column_types = column_types
@@ -14,7 +183,7 @@ class Validator(BaseEstimator, TransformerMixin):
         self.value_ranges = value_ranges
         #non_negative_values:list
         #List of columns that should be not negative
-        self.non_negative_values = non_negative_values
+        self.non_negative_columns = non_negative_columns
         #check_missing: bool
         #Whether to check missing values in dataset
         self.check_missing = check_missing
@@ -60,26 +229,26 @@ class Validator(BaseEstimator, TransformerMixin):
                 raise ValueError(f'There is no column {col} in dataset')
         
     #Check missing values 
-    def check_missing(self, X):
+    def _check_missing_values(self, X):
         
         missing_columns = X.columns[X.isna().any()].tolist()
         if missing_columns:
             raise ValueError(f'The following columns have missing values: {missing_columns}')
         
     #check duplicates
-    def duplicates(self):
+    def _check_duplicates(self, X):
         
-        if X.duplicated.any():
+        if X.duplicated().any():
             raise ValueError('The dataset contains duplicated rows')
 
     
     #Fitting model
-    def fit(self, X):
+    def fit(self, X, y = None):
 
         if self.column_types is None:
-            self.column_types = {col: X[col].dtype for col in X.columns}
+            self.column_types = X.dtypes.to_dict()
         if self.value_ranges is None:
-            self.value_ranges = {col: (X.col.min(), X.col.max()) for col in X.select_dtypes(include = [np.number])}
+            self.value_ranges = {col: (X[col].min(), X[col].max()) for col in X.select_dtypes(include = [np.number])}
         if self.non_negative_columns is None:
             self.non_negative_columns = [col for col in X.select_dtypes(include = [np.number])]
 
@@ -89,21 +258,23 @@ class Validator(BaseEstimator, TransformerMixin):
     def transform(self, X):
 
         if self.column_types:
-            self._check_column_types()
+            self._check_column_types(X)
 
         if self.value_ranges:
-            self._check_value_ranges()
+            self._check_value_ranges(X)
 
-        if self.non_negative_values:
-            self._check_non_negative_values()
+        if self.non_negative_columns:
+            self._check_non_negative_values(X)
 
         if self.check_duplicates:
-            self._check_duplicates()
+            self._check_duplicates(X)
         
         if self.check_missing:
-            self._check_missing_values()
+            self._check_missing_values(X)
         
         return X
+
+
         
                                                
 
