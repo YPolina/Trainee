3663a0448632974c5b2a8bc1ef30c138e12a88cb
«YPolina» <yatsko_polina1@mail.ru>
2024-11-29 23:26:12 +0300

    Bug Fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="future_sales_prediction_2024",
-    version="1.2.4",
+    version="1.2.5",
     description="A package for feature extraction, hyperopt, and validation schemas",
     long_description=long_description,
     long_description_content_type = "text/markdown",
