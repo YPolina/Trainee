09fc03e1dc674a9d4a3685668e0dbd464f727e7a
«YPolina» <yatsko_polina1@mail.ru>
2024-12-10 14:56:24 +0300

    DS-4.1 Changes: - docker-compose is implemented - requirements.txt is created for docker-specific packages

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -e  # Exit on errors
+
 # Authenticate with Google Cloud
-echo "Authenticating with Google Cloud..."
-gcloud auth activate-service-account --key-file=/opt/airflow/key.json
-gcloud auth application-default login
+echo "Authenticating with Google Cloud using service account key..."
+export GOOGLE_APPLICATION_CREDENTIALS=/opt/airflow/key.json
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+
+# Navigate to the repository directory
+echo "Navigating to the repository directory..."
+cd /repository || exit 1
 
 # Pull data with DVC
 echo "Pulling data with DVC..."
@@ -13,10 +19,11 @@ if ! dvc pull; then
 fi
 
 # Initialize Airflow database
-echo "Initializing Airflow..."
-airflow db init
+echo "Initializing Airflow database..."
+airflow db migrate
 
-# Create the admin user
+# Create an admin user
+echo "Creating Airflow admin user..."
 airflow users create --username admin --password admin --firstname Admin --lastname User --role Admin --email palinayatsko@innowise.com
 
 # Start the Airflow webserver
